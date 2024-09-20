6fe227226b23e1e7001deb0e34afde418541ee94
William <willian_alves96@outlook.com>
2024-09-19 22:24:16 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+
+
+  return <>
+<header> cabeçalho </header>
+<aside> lado-esq</aside>
+<footer>rodapé </footer>
+<main> principal </main>
+  </>
+   
+  
+}
+
+export default App
